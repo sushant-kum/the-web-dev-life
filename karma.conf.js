95e4bb5762b4753d70c4e473ddaf189d264b8976
Sushant Kumar <sushant.kum96@gmail.com>
2022-05-25 11:41:48 +0530

    build: setup dev environment partially

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-boilerplate'),
+      dir: require('path').join(__dirname, './coverage/the-web-dev-life'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-boilerplate'`, () => {
+  it(`should have as title 'the-web-dev-life'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-boilerplate');
+    expect(app.title).toEqual('the-web-dev-life');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-boilerplate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('the-web-dev-life app is running!');
   });
 });
